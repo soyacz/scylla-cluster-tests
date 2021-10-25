1e6269036759afd8c1e3395fe9a2c47295af52e6
Lukasz Sojka <lukasz.sojka@scylladb.com>
2021-10-25 08:56:02 +0200

    refactor

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,7 +2,7 @@ from invoke import Collection
 
 import tasks.sample
 import tasks.sct
-from tasks.persisted_context import PersistedContext
+from tasks.libs.persisted_dicts import FilePersistedDotDict
 
 ns = Collection(tasks.sample, tasks.sct)
-ns.configure({'persisted': PersistedContext()})
+ns.configure({'persisted': FilePersistedDotDict("persisted_dict.json")})
--- tasks/sample.py
+++ tasks/sample.py
@@ -1,20 +1,47 @@
 # pylint: disable=W,C,R
+from enum import Enum
 from time import sleep
+from typing import List, Union
 
 from invoke import task, Collection
-
-
 from rich import print  # pylint: disable=redefined-builtin
 from rich.console import Console
 
-from tasks.persisted_context import PersistedContext
+from tasks.libs.config import Settings
 
 console = Console(color_system=None)
+
+
+class Backends(str, Enum):
+    aws: str = "aws"
+    gce: str = "gce"
+
+
+class SampleSettings(Settings):
+    backend: Backends
+    aws_region: Union[str, List]
+    scylla_ami_id: str
+    gce_image_db: str
+    update_db_packages: str
+    scylla_version: str
+    scylla_repo: str
+    scylla_mgmt_agent_version: str = ""
+    scylla_mgmt_address: str
+
+
+@task
+def configure(ctx):
+    print("preparing configuration")
+    settings = SampleSettings()
+    config = ctx.persisted
+    print(settings.dict())
+    config.update(**settings.dict())
 
 
 @task
 def clean(ctx):
     print("cleaning...")
+    print(f"param from configuration: {ctx.persisted.aws_region}")
     ctx.run("ls test_lib")
     sleep(0.5)
     print("cleaning complete!")
@@ -24,18 +51,17 @@ def clean(ctx):
 def build(ctx):
     print("started building...")
     print(f"Setting new context param 'something' to 'test'")
-    ctx.persisted.params["something"] = "test"
+    ctx.persisted.something = "test"
     with open("build_output.txt", "w") as build_out_file:
         ctx.run("lscpu", out_stream=build_out_file)
     sleep(1)
-    ctx.persisted.save()
     print("build complete!")
 
 
 @task
 def test(ctx):
     print("started tests...")
-    print(f'Getting new context param "something": {ctx.persisted.params["something"]}')
+    print(f'Getting new context param "something": {ctx.persisted.something}')
     sleep(1)
     print("tests complete!")
 
@@ -47,12 +73,9 @@ def package(ctx):
     print("packaging complete!")
 
 
-@task(clean, build, test, package)
+@task(configure, clean, build, test, package)
 def all_tasks(ctx):
     print("hello world!")
-    print(f'jenkins params: {ctx.persisted.params=}')
-    # print(f'param {ctx["params"]["aws_region"]=}')
-    ctx["persisted"].save()
 
 
-ns = Collection(all_tasks, clean, build, test, package)
+ns = Collection(all_tasks, configure, clean, build, test, package)
