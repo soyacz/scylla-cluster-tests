d929eeeb9b299f1139462e58914bc1a0242138ae
Lukasz Sojka <lukasz.sojka@scylladb.com>
2022-04-22 19:08:13 +0200

    feature(azure): make provision in parallel

--- sdcm/provision/azure/ip_provider.py
+++ sdcm/provision/azure/ip_provider.py
@@ -14,7 +14,7 @@
 import logging
 from dataclasses import dataclass, field
 
-from typing import Dict
+from typing import Dict, List
 
 from azure.core.exceptions import ResourceNotFoundError
 from azure.mgmt.network.models import PublicIPAddress
@@ -41,7 +41,39 @@ class IpAddressProvider:
         except ResourceNotFoundError:
             pass
 
-    def get_or_create(self, name: str = "default", version: str = "IPV4") -> PublicIPAddress:
+    def get_or_create(self, names: List[str] = "default", version: str = "IPV4") -> List[PublicIPAddress]:
+        addresses = []
+        pollers = []
+        for name in names:
+            ip_name = self._get_ip_name(name, version)
+            if ip_name in self._cache:
+                addresses.append(self._cache[ip_name])
+                continue
+            LOGGER.info("Creating public_ip %s in resource group %s...",  ip_name, self._resource_group_name)
+            poller = self._azure_service.network.public_ip_addresses.begin_create_or_update(
+                resource_group_name=self._resource_group_name,
+                public_ip_address_name=ip_name,
+                parameters={
+                    "location": self._region,
+                    "sku": {
+                        "name": "Standard",
+                    },
+                    "public_ip_allocation_method": "Static",
+                    "public_ip_address_version": version.upper(),
+                },
+            )
+            pollers.append((ip_name, poller))
+        for ip_name, poller in pollers:
+            poller.wait()
+            # need to get it separately as seems not always it gets created even if result() returns proper ip_address.
+            address = self._azure_service.network.public_ip_addresses.get(self._resource_group_name, ip_name)
+            LOGGER.info("Provisioned public ip %s (%s) in the %s resource group", address.name,
+                        address.ip_address, self._resource_group_name)
+            self._cache[ip_name] = address
+            addresses.append(address)
+        return addresses
+
+    def get_or_create_one(self, name: str = "default", version: str = "IPV4") -> PublicIPAddress:
         ip_name = self._get_ip_name(name, version)
         if ip_name in self._cache:
             return self._cache[ip_name]
